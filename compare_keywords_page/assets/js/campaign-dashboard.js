5f1964d4e778ce6f6e635ceb6f0f85137bf68ae2
bruno <elochux@gmail.com>
2020-08-21 18:45:11 +0100

    chore: refresher

--- compare_keywords_page/assets/js/campaign-dashboard.js
+++
@@ -1,8 +0,0 @@
-const togglers = document.querySelectorAll(".sidebar-toggler");
-togglers.forEach((toggler) => {
-  toggler.addEventListener("click", (e) => {
-    document
-      .querySelector("aside.sidebar")
-      .parentElement.classList.toggle("collapsed-sidebar");
-  });
-});
